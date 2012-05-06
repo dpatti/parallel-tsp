d75da99030851c8fd5246abd9ad9eff77b8ebf7c
Doug Patti <douglas.patti@gmail.com>
2012-05-06 19:35:37 -0400

    Updated bluegene script

--- blue_gene_run.sh
+++ blue_gene_run.sh
@@ -4,10 +4,17 @@ BGLMPI_MAPPING=TXYZ
 INDEX=0
 
 for i in $@; do
-	mpirun \
-		-mode VN \
-		-np $i \
-		-cwd /gpfs/small/PCP2/home/PCP2patt/project \
-		./aco --graph_size 128 --iterations 500 \
-        > ./out/stdout-$i-$SLURM_JOB_ID 2> ./out/stderr-$i-$SLURM_JOB_ID
+    for graph in 1024; do
+        for ants in 128; do
+            for iter in 500; do
+                mpirun \
+                    -mode VN \
+                    -np $i \
+                    -cwd /gpfs/small/PCP2/home/PCP2patt/project \
+                    ./aco --graph_size $graph --ants $ants --iterations $iter \
+                     > ./out/stdout-$i-$graph-$ants-$iter-$SLURM_JOB_ID \
+                    2> ./out/stderr-$i-$graph-$ants-$iter-$SLURM_JOB_ID
+            done
+        done
+    done
 done
